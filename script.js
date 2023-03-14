cd2b0db7b669f4b699093440058224825147ee72
SandraDeve <sandra.wasielke@gmail.com>
2023-03-14 19:06:58 +0000

    code corrections, adding  isNaN() function

--- script.js
+++ script.js
@@ -88,41 +88,51 @@ var upperCasedCharacters = [
   "Z",
 ];
 
-// Function to prompt user for password options
-
-//function prompts the user to input the desired length of their password. If the user enters a number less than 8 or greater than 128, the function will continue to prompt the user until a valid length is entered. Once a valid length is entered//
-function getPasswordOptions()
-var passwordLength = prompt("How many characters would you like your password to contain? (between 8 and 128 characters)");
-while (passwordLength < 8 || passwordLength > 128) {
-  passwordLength = prompt("Password length must be between 8 and 128 characters. How many characters would you like your password to contain?");
-}
-var includeLowercase = confirm("Click OK to include lowercase characters.");
-var includeUppercase = confirm("Click OK to include uppercase characters.");
-var includeNumeric = confirm("Click OK to include numeric characters.");
-var includeSpecial = confirm("Click OK to include special characters.");
-
-// This while loop will keep running as long as the user has not selected any character types for their password. It uses a series of confirm() functions to prompt the user to select whether they want to include lowercase, uppercase, numeric, and/or special characters//
-
-while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
-  alert("Please select at least one character type.");
-  includeLowercase = confirm("Click OK to include lowercase characters.");
-  includeUppercase = confirm("Click OK to include uppercase characters.");
-  includeNumeric = confirm("Click OK to include numeric characters.");
-  includeSpecial = confirm("Click OK to include special characters.");
-}
-//This part of the code is an empty object called passwordOptions, then assigns values to its properties one at a time. The parseIn function is used to convert the passwordLength string to an integer before assigning it to the length property.//
-var passwordOptions = {};
-passwordOptions.length = parseInt(passwordLength);
-passwordOptions.includeLowercase = includeLowercase;
-passwordOptions.includeUppercase = includeUppercase;
-passwordOptions.includeNumeric = includeNumeric;
-passwordOptions.includeSpecial = includeSpecial;
-
 // Function for getting a random element from an array
 function getRandom(arr) {}
 
 // Function to generate password with user input
-function generatePassword() {}
+function generatePassword() {
+  var passwordLength;
+  var containLowercase;
+  var containUppercase;
+  var containNumeric;
+  var containSpecial;
+
+  // Function to prompt user for password options
+
+  // get password length
+  passwordLength = prompt(
+    "How many characters would you like your password to contain? (between 8 and 128 characters)"
+  );
+  // converting passwordlength from string to number
+  passwordLength = parseInt(passwordLength);
+
+  // check passwordLength validity
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please choose a number between 8 and 128 characters");
+    return generatePassword();
+  }
+  containLowercase = confirm("Click OK to include lowercase characters.");
+  containUppercase = confirm("Click OK to include uppercase characters.");
+  containNumeric = confirm("Click OK to include numeric characters.");
+  containSpecial = confirm("Click OK to include special characters.");
+
+  console.log(passwordLength);
+
+  // This while loop will keep running as long as the user has not selected any character types for their password. It uses a series of confirm() functions to prompt the user to select whether they want to include lowercase, uppercase, numeric, and/or special characters//
+  if (
+    !containLowercase &&
+    !containUppercase &&
+    !containNumeric &&
+    !containSpecial
+  ) {
+    alert("Please select at least one character type.");
+    return generatePassword();
+  }
+  //This part of the code is an empty object called passwordOptions, then assigns values to its properties one at a time. The parseIn function is used to convert the passwordLength string to an integer before assigning it to the length property.//
+  var finalPasswordCharacterPool = [];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
